29f4e35c7ba6e695ab9864675784d16c94794044
João Felipe <joaofsp.games@gmail.com>
2025-01-13 18:43:11 -0300

    Udemy(TailwindCSS Foundamentals): Width, Height, Padding, Margin, Text, color, Background-color e box-shadow

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaria : "#FF00FF"
+      }
+    },
+  },
+  plugins: [],
+}
+
